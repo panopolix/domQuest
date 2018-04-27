d7fc07e4dbe5cfc55bb1ddb17e19b1f5f1c68e75
Jeffrey Klier <jeffrey.klier@galvanize.com>
2018-04-26 17:20:47 -0700

    updated js to use querySelector and added notes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-let cashLabel = document.getElementById('cashTotal');
+//document.querySelector() allows us to access the FIRST HTML element on the page that can be identified with the given CSS selector.
+//document.querySelectorAll() will give us an array-like LIST of elements that can be identified with the given CSS selector.
 
-let smallQuestButton = document.getElementById('smallQuest');
-let longQuestButton = document.getElementById('longQuest');
-let bossFightButton = document.getElementById('bossFight');
+let cashLabel = document.querySelector('#cashTotal');
 
+let smallQuestButton = document.querySelector('#smallQuest');
+let longQuestButton = document.querySelector('#longQuest');
+let bossFightButton = document.querySelector('#bossFight');
 
+//The addEventListener() function allows to run a javascript function when a specific event occurs on the HTML page (such as a mouse-click, or a hover over).
+//addEventListener() takes two arguments, the first being a string that specifies what event we want to "listen" to, and the second being the function we want to run.
+//Notice that the function passed in as an argument is not invoked (aka there's no parenthesis next to the name), but rather the function name alone is given.
 smallQuestButton.addEventListener('click', takeSmallQuest);
 longQuestButton.addEventListener('click', takeLongQuest);
 bossFightButton.addEventListener('click', takeBossFight);
